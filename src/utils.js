28a4540d7f095ba25473b88782f5ca9e38df605b
J-81 <jonathan.d.oribello@gmail.com>
2022-04-08 09:49:14 -0700

    Conversion to typescript. Adding comments.

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,12 @@
-function calculateWinner(squares) {
+// @ts-check
+
+/**
+ * Determines if a player has won
+ * A winner is defined as a player who places a line of that same characters
+ * @param squares 
+ * @returns 
+ */
+function calculateWinner(squares: Array<string>): string | null {
     const lines = [
       [0, 1, 2],
       [3, 4, 5],
@@ -9,8 +17,12 @@ function calculateWinner(squares) {
       [0, 4, 8],
       [2, 4, 6],
     ];
+    // iterate through all lines
     for (let i = 0; i < lines.length; i++) {
       const [a, b, c] = lines[i];
+      // check if the first position is populated
+      // And the first position and second position are the same char
+      // Then if the first and third position are the same char
       if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
         return squares[a];
       }
